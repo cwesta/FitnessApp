abbbba31f2b219e3c43cd548d0b521809846ce66
chuck <cwestca@gmail.com>
2022-01-12 16:30:29 -0800

    Added fitness Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect } from "react";
+
+
+const handleRunTraining = () => {
+  console.log('Run training');
+}
 
 function App() {
+
+  // useEffect(() => {
+
+  // }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+     <div className='training_button'>
+       <button onClick={handleRunTraining}>Run Training!</button>
+     </div>
+
     </div>
   );
 }
