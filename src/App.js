cc1be02a1bca92d2a7cb6ca2fff0ca274fb13de6
chuck <cwestca@gmail.com>
2022-02-02 21:37:22 -0800

    fixed tensorflow/js library issue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import react, { useEffect } from "react";
+import * as tf from '@tensorflow/tfjs';
 
 
 const handleRunTraining = () => {
   console.log('Run training');
+
+}
+
+const displayTensor = () => {
+  let example = tf.tensor2d([ [1,2,3], [3,5,6] ]);
+  console.log(example);
 }
 
 function App() {
 
-  // useEffect(() => {
 
-  // }, [])
 
   return (
     <div className="App">
       <br />
      <div className='training_button'>
        <button onClick={handleRunTraining}>Run Training!</button>
+       <button onClick={displayTensor}>Display Tensor!</button>
      </div>
 
     </div>
